22b2e0021e9cebd14cce95020579bf69b22b723e
masinc <masinc000@gmail.com>
2021-01-10 20:23:58 +0900

    add: board

--- src/App.tsx
+++ src/App.tsx
@@ -1,11 +1,11 @@
 import React from "react";
 import logo from "./logo.svg";
 import "./App.css";
-
+import Board from "./Board";
 function App() {
   return (
     <div className="App">
-      <div className="Board"></div>
+      <Board />
     </div>
   );
 }
---
+++ src/Board.tsx
@@ -0,0 +1,8 @@
+import React from "react";
+import "./Board.css";
+
+function Board() {
+  return <div className="Board"></div>;
+}
+
+export default Board;
